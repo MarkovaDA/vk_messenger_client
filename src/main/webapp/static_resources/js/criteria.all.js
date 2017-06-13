5aa601af37d7edf31a62ed6760039e3eec62b5b9
darya_markova <darya.markova.95@mail.ru>
2017-06-13 18:30:59 +0300

    отображение критериев полностью функционирует

--- src/main/webapp/static_resources/js/criteria.all.js
+++ src/main/webapp/static_resources/js/criteria.all.js
@@ -2,21 +2,17 @@
 $(document).ready(function(){
    
    $('#select_company').on('hide.bs.select', function(){
-       console.log('hide bs select', $('#txt_company_code').val());
-        //getAllCriteria($('#txt_company_code').val());
-        
-        $('#all_criteria .panel-body .separate_criteria').remove();
+        getAllCriteria($('#txt_company_code').val());
+        /*$('#all_criteria .panel-body .separate_criteria').remove();
         var cloned_block = $('.separate_criteria');
         var criteria_count = randomInteger(2,10);//имитирует число критериев
         console.log(criteria_count);
         for (var i=0; i < criteria_count; i++) {
             cloned_block = cloned_block.clone();
+            cloned_block.find('input').val(i);
             $('#all_criteria .panel-body').append(cloned_block);
-        }
-        //$('#all_criteria .panel-body').append(cloned_block);
-        //$('#all_criteria .panel-body').append(cloned_block.clone());
-        //$('#all_criteria .panel-body').append(cloned_block);      
-        $('.panel-body .separate_criteria').show();
+        }    
+        $('.panel-body .separate_criteria').show();*/
    });
 });
 function getAllCriteria(company_code){
@@ -27,18 +23,21 @@ function getAllCriteria(company_code){
         'contentType' : "application/json",
         'type': 'GET',
         'url': 'company/criteria/'+ company_code,
-        success: function(data){    
-            //console.log(data);
-            var cloned_block = $('.separate_criteria').clone();
-            $('#all_criteria.panel').empty();
-            //cloned_block.find('input').val("олоол");
-            //cloned_block.attr('criteria_id',2);
-            //пока вставка не выполняется
-            $('#all_criteria.panel').append(cloned_block);
-            $('#all_criteria.panel').append(cloned_block);
-            //showMessage('alert-info',data);
+        success: function(data){
+            console.log(data);
+            $('#all_criteria .panel-body .separate_criteria').remove();
+            var cloned_block = $('.separate_criteria');
+            for(index in data){
+                var item = data[index];
+                console.log(item["title"]);
+                cloned_block = cloned_block.clone();
+                cloned_block.find('input').val(item["title"]);
+                $('#all_criteria .panel-body').append(cloned_block);                
+            }
+            $('.panel-body .separate_criteria').show();
         },
         error:function(jqXHR, textStatus, errorThrown){
+            
             console.log(jqXHR, textStatus, errorThrown);
             //showMessage('alert-danger', xhr.responseJSON);
         }
